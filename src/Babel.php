9b895332025626c4d077514c9a651cfe5d24a7bf
Kolesár András <kolesar@kolesar.hu>
2015-02-12 14:22:52 +0100

    gpsbabel-műveletek különválasztása

---
+++ src/Babel.php
@@ -0,0 +1,42 @@
+<?php namespace KolesarAndras\TuristautakOsmTrackImport;
+
+/**
+ * nyomvonalak átalakítása
+ *
+ * @package kolesar-andras/turistautak-osm-track-import
+ * @url https://github.com/kolesar-andras/turistautak-osm-track-import
+ *
+ * @author Kolesár András <kolesar@turistautak.hu>
+ * @since 2015.02.12
+ *
+ */
+
+class Babel {
+
+	function process ($id, $in, $out, $args=null) {
+		$format = Format::formatForFilename($in);
+		if ($format == '')
+			throw new \Exception('Unknown format for file: ' . $in);
+
+		$storage = new Storage($id);
+		$dir = $storage->dir();
+		$cmd = sprintf('gpsbabel -i %s -f %s -o gpx -F %s %s 2>&1',
+			escapeshellarg($format),
+			escapeshellarg($dir . $in),
+			escapeshellarg($dir . $out),
+			$args);
+
+		$output = shell_exec($cmd);
+		
+		if ($output != '') {
+			$storage->put('gpsbabel.log', $output);
+			echo $cmd, "\n";
+			echo $output;
+			throw new \Exception('gpsbabel printed errors');
+		} else {
+			$storage->delete('gpsbabel.log');
+		}
+	}
+}
+
+
--- src/Convert.php
+++ src/Convert.php
@@ -19,24 +19,8 @@ class Convert extends Task {
 
 		$storage = new Storage($id);
 		$data = $storage->getData('turistautak');
-		$dir = $storage->dir();
 		foreach ($data['files'] as $file) {
-			$format = Format::formatForFilename($file['name']);
-			if ($format == '')
-				throw new \Exception('Unknown format for file: ' . $file['name']);
-			$cmd = sprintf('gpsbabel -i %s -f %s -o gpx -F %s 2>&1',
-				escapeshellarg($format),
-				escapeshellarg($dir . $file['name']),
-				escapeshellarg($dir . $file['name'] . '.gpx'));
-			$output = shell_exec($cmd);
-			if ($output != '') {
-				$storage->put('gpsbabel.log', $output);
-				echo $cmd, "\n";
-				echo $output;
-				throw new \Exception('gpsbabel printed errors');
-			} else {
-				$storage->delete('gpsbabel.log');
-			}
+			Babel::process($id, $file['name'], $file['name'] . '.gpx');
 		}
 	}
 }
